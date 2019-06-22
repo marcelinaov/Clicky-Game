467a8bb73f9516881b536e4465cf9eaee7a5e203
marcelinaov <anaortiz@anas-mbp.uguest.utah.edu>
2019-06-22 11:32:34 -0600

    Updates

---
+++ precache-manifest.655831c29b3aaba883c1f2779d2a9ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4842fb6f241d1b87b6712d18c52c92a9",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "8ec7d4dd8aff0ef232f9",
+    "url": "/Clicky-Game/static/css/main.5e1d5275.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/Clicky-Game/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "8ec7d4dd8aff0ef232f9",
+    "url": "/Clicky-Game/static/js/main.df318608.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
